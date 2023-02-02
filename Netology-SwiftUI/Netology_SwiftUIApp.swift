802fad576b98c587c0359b8bd795ec4e927cb279
Aleksei Serdiuk <aleksey.serduyk@gmail.com>
2023-02-02 14:52:15 +0600

    innitial commit

---
+++ Netology-SwiftUI/Netology_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Netology_SwiftUIApp.swift
+//  Netology-SwiftUI
+//
+//  Created by Алексей Сердюк on 02.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct Netology_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
